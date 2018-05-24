eed0f47ef7b62e542f57d413380c500a5ddc6ef3
AshwiniBothe <39567067+AshwiniBothe@users.noreply.github.com>
2018-05-24 18:46:49 +0530

    Create RealTimeStatisticsApplication.java

---
+++ src/main/java/com/ashwini/realtimestatistics/RealTimeStatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.ashwini.realtimestatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeStatisticsApplication.class, args);
+	}
+}
