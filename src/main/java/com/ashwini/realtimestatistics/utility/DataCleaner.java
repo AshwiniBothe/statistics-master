93e801504bab9aff3b5df40e8c7dc87814bedcfd
AshwiniBothe <39567067+AshwiniBothe@users.noreply.github.com>
2018-05-24 22:22:20 +0530

    Update DataCleaner.java

--- src/main/java/com/ashwini/realtimestatistics/utility/DataCleaner.java
+++ src/main/java/com/ashwini/realtimestatistics/utility/DataCleaner.java
@@ -1,6 +1,6 @@
 package com.ashwini.realtimestatistics.utility;
 
-import com.ashwini.realtimestatistics.services.TransactionService;
+import com.ashwini.realtimestatistics.services.RealTimeTransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,10 +12,10 @@ public class DataCleaner {
     private static final long ONE_MINUTE_IN_MILLISECOND = 60 * 1000l;
 
     @Autowired
-    TransactionService transactionService;
+    RealTimeTransactionService realTimeTransactionService;
 
     @Scheduled(fixedRate = 1000)
     public void cleanOldData() {
-        transactionService.deleteOldTransactions();
+        realTimeTransactionService.deleteOldTransactions();
     }
 }
