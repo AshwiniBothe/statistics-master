007ef6b9f2e97648d0cc5ec61c1a3af0798be8f2
AshwiniBothe <39567067+AshwiniBothe@users.noreply.github.com>
2018-05-24 18:44:32 +0530

    Create Transaction.java

---
+++ src/main/java/com/ashwini/realtimestatistics/model/Transaction.java
@@ -0,0 +1,26 @@
+package com.ashwini.realtimestatistics.model;
+
+import java.io.Serializable;
+
+
+public class Transaction implements Serializable{
+
+    private Double amount;
+    private Long timestamp;
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
